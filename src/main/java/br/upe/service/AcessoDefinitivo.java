b855de698caa7f843124d634a29eb4f61bbc8fc6
Bruno Baier <brunobaier3@gmail.com>
2024-11-29 16:18:27 -0300

    Modificando a classe AcessoDefinitivo

--- src/main/java/br/upe/service/AcessoDefinitivo.java
+++ src/main/java/br/upe/service/AcessoDefinitivo.java
@@ -2,4 +2,10 @@ package br.upe.service;
 public class AcessoDefinitivo {
 	// TODO: Método para abrir um arquivo
 	// TODO: Método para instaciar um database na mémoria
+	//
+	public void databaseAcesso(String[] database){
+		// TODO: Esse método pega a do o array e entra nele
+		// Usa o try para acessar e o finally para fechar o arquivo
+		// Se ele não existir tu lança um exeção dizendo que não existe
+	}
 }
