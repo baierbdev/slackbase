c0fec9b734b9d91a0ca706d20ff186e4d4bb1791
Bruno Baier <brunobaier3@gmail.com>
2024-11-29 18:07:50 -0300

    Modificando a classe de analise lexica

--- src/main/java/br/upe/Service/AnalisadorLexico.java
+++ src/main/java/br/upe/Service/AnalisadorLexico.java
@@ -5,7 +5,40 @@ public class AnalisadorLexico {
 	 * recebidas apartir da entrada
 	 */
 
-	public void analisadorDeRotinas(String[] rotina){
-					
-	}    
+	public static void analisadorDeRotinas(String[] rotina){
+		for(String s : rotina){
+			System.out.println(s);
+		}
+
+		// primeiroComando(rotina[0]);
+		// segundoComando(rotina[1]);
+
+
+	}
+
+	public static void primeiroComando(String cmd) throws Exception {
+		String[] acoes = {"create", "delete", "update", "read", "describe", "help", "info"};
+		try {
+
+			for(String acao : acoes){
+				if(!acao.equals(cmd)){
+					continue;
+				} else{
+					throw new Exception();
+				}
+			}
+		} catch (Exception e){
+			System.out.println("Comando inválido");
+		}
+
+
+	}
+
+	public static void segundoComando(String database){
+		if(database.equals("database")){
+
+		}
+
+
+	}
 }
