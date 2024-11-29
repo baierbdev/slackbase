1603d0af0b06604f86997b03acbd6c1464ec2e0a
Bruno Baier <brunobaier3@gmail.com>
2024-11-29 17:03:17 -0300

    Refatorando o código e adicionando novos métodos

--- src/main/java/br/upe/service/AcessoDefinitivo.java
+++ src/main/java/br/upe/service/AcessoDefinitivo.java
@@ -2,10 +2,22 @@ package br.upe.service;
 public class AcessoDefinitivo {
 	// TODO: Método para abrir um arquivo
 	// TODO: Método para instaciar um database na mémoria
-	//
+
+
+	public static boolean verificadoDeExistencia(String[] stringArray) {
+		/* Esse método recebe o array de strings da Classe 
+		 * StringManipulator e verifica se o database passado existe
+		 */
+		return false;
+	}
 	public void databaseAcesso(String[] database){
-		// TODO: Esse método pega a do o array e entra nele
-		// Usa o try para acessar e o finally para fechar o arquivo
-		// Se ele não existir tu lança um exeção dizendo que não existe
+		/* Esse método pega o nome do database do o array e entra nele
+		* Usa o try para acessar e o finally para fechar o arquivo
+		*/
+	}
+	public void localPadrao(String nomeDoSistema){
+		/* Esse método pega o nome do sistema da classe Sistema operacional
+		 * e sobe na pasta padrão de seu respectivo sistema
+		 */
 	}
 }
--- src/main/java/br/upe/service/SistemaOperacional.java
+++ src/main/java/br/upe/service/SistemaOperacional.java
@@ -2,5 +2,10 @@ package br.upe.service;
 
 public class SistemaOperacional {
 	// TODO: Implementar classes para portabilidade
+	private String os;
+
+	public String getOperatingSystem(){
+		return System.getProperty("os.name");
+	}
 }
 
