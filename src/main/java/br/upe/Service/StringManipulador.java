ec1f952f0b2572116c5295eff4d7e7ceae92fcd4
Lucas Terêncio <lucas.tnsantos@upe.br>
2024-11-30 20:06:25 -0300

    alteração regex string manipulador

--- src/main/java/br/upe/Service/StringManipulador.java
+++ src/main/java/br/upe/Service/StringManipulador.java
@@ -3,7 +3,7 @@ package br.upe.Service;
 
 public class StringManipulador {
     public static String[] quebrarStringEspaco(String string) throws Exception {
-        String regex = "[,\\.\\s]";
+        String regex = " (?=(?:[^']*'[^']*')*[^']*$)(?=(?:[^()]*\\([^()]*\\))*[^()]*$)";
         String[] strArr = string.split(regex);
         AnalisadorLexico.analisadorDeRotinas(strArr);
 
