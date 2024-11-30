c656499cac2ba0fe457d4478042c38aa85329bae
Lucas Terêncio <lucas.tnsantos@upe.br>
2024-11-30 17:30:24 -0300

    Adicionando metodos de EntradaESaida

--- src/main/java/br/upe/IO/EntradaESaida.java
+++ src/main/java/br/upe/IO/EntradaESaida.java
@@ -1,9 +1,14 @@
 package br.upe.IO;
 
+import br.upe.Service.StringManipulador;
+
+import static br.upe.Service.StringManipulador.quebrarString;
+
 public class EntradaESaida {
-    public void receberEntrada(String entrada ){
+    public static void receberEntrada(String entrada ) throws Exception {
 	/* Método que tem como função enviara entrada 
 	 * para as classes de serviços
 	 */
+        StringManipulador.quebrarString(entrada);
     }
 }
