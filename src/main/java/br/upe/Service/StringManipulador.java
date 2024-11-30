af248e8fc30810fe81bc1782129c308ccf085b75
Lucas Terêncio <lucas.tnsantos@upe.br>
2024-11-30 19:48:41 -0300

    fixing

--- src/main/java/br/upe/Service/StringManipulador.java
+++ src/main/java/br/upe/Service/StringManipulador.java
@@ -2,7 +2,7 @@ package br.upe.Service;
 
 
 public class StringManipulador {
-    public String[] quebrarStringEspaco(String string) throws Exception {
+    public static String[] quebrarStringEspaco(String string) throws Exception {
         String regex = "[,\\.\\s]";
         String[] strArr = string.split(regex);
         AnalisadorLexico.analisadorDeRotinas(strArr);
