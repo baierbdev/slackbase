b70df5e5c0040b7e74a7c782ae05a1f2e83067e1
Bruno Baier <brunobaier3@gmail.com>
2024-11-30 17:49:16 -0300

    Modidificações na classe principal

--- src/main/java/br/upe/App.java
+++ src/main/java/br/upe/App.java
@@ -1,27 +1,34 @@
 package br.upe;
 import br.upe.IO.EntradaESaida;
-
+import br.upe.SO.SistemaOperacional;
+import java.util.NoSuchElementException;
 import java.util.Scanner;
 
 public class App {
 
-    public static void main(String[] args) throws Exception {
-        // TODO: Fazer a checagem do sistema operacional 
-	Scanner scanner = new Scanner(System.in);
-        while( true ) {
-            System.out.printf("> ");
-            String entrada = scanner.nextLine();
-            if (entrada.equals("exit")) {
-                System.out.println("Bye!! ");
-                scanner.close();
-                break;
-            }
+	public static void main(String[] args) throws Exception {
+		// TODO: Fazer a checagem do sistema operacional 
+		SistemaOperacional.setaSistemaConfig();
+		try{
+			Scanner scanner = new Scanner(System.in);
+			while( true ) {
+				System.out.printf("> ");
+				String entrada = scanner.nextLine();
+				if (entrada.equals("exit")) {
+					System.out.println("Bye!! ");
+					scanner.close();
+					break;
+				}
 
-            EntradaESaida.receberEntrada(entrada);
+				EntradaESaida.receberEntrada(entrada);
 
-        }
+			}
+		} catch (NoSuchElementException e){
+			System.out.println("Não há nenhuma entrada");
+		} catch (ArrayIndexOutOfBoundsException e){
+			System.out.println("Não há nenhuma entrada");
+		}
 
 
-
-    }
+	}
 }
